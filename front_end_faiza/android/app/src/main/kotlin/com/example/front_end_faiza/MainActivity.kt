5ac6fa41eb091c4f9bbf85286c34ac9af549f3a4
Faiza Rahman <faizarahman301@gmail.com>
2022-03-01 22:08:20 -0600

    loading screen complete

---
+++ front_end_faiza/android/app/src/main/kotlin/com/example/front_end_faiza/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.front_end_faiza
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ front_end_faiza/lib/main.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: prefer_const_constructors
+
+//import 'package:brightowl_app/screens/onboardingpageScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:easy_splash_screen/easy_splash_screen.dart';
+
+void main() {
+  runApp(const SplashScreen());
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          body: EasySplashScreen(
+        logo: Image.asset('assets/images/dog_logo_transparent.png'),
+        logoSize: 120,
+        title: Text(
+          "Ready, Pet, Go!",
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        showLoader: true,
+        loadingText: Text("Loading..."),
+        //redirect to page you want it to go
+        //navigator: OnboardingScreen(),
+        durationInSeconds: 5,
+      )),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
