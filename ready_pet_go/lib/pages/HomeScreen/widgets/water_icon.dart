2a3ce9618940ae4242bd4ad28ed5ac91b50eedea
ManishMallik <manish03mallik@gmail.com>
2022-04-06 18:33:16 -0500

    Changing the water drop icon

--- ready_pet_go/lib/pages/HomeScreen/widgets/water_icon.dart
+++ ready_pet_go/lib/pages/HomeScreen/widgets/water_icon.dart
@@ -16,7 +16,7 @@ class WaterIcon extends StatelessWidget{
           width: 65.0,
           height: 64.0,
           child: Image.asset(
-            "assets/images/91c294f141f9a162e40c82c18c20285830a2fc63.png",
+            "assets/images/raindrop.png",
             color: null,
             width: 30,
             height: 41,
