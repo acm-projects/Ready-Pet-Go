dfa2009242b33da04fbc6c22977e9056b45a4dac
Jason Nguyen <trungnt281@gmail.com>
2022-04-14 18:10:45 -0500

    Modify main

--- ready_pet_go/lib/main.dart
+++ ready_pet_go/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:ready_pet_go/front-end/screens/start_walk_screen.dart';
 import 'package:ready_pet_go/front-end/screens/login_screen.dart';
 
 import 'firebase_options.dart';
@@ -11,8 +10,8 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MaterialApp(
-    title: 'Firebase Demo',
+    title: 'Ready Pet Go',
     home: LoginScreen(),
+    debugShowCheckedModeBanner: false,
   ));
 }
-
